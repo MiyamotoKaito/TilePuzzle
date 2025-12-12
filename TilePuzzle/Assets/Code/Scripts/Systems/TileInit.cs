f49a0f038519f75f38fa3e7d2c6ede806d2bb677
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-12 19:34:27 +0900

    [add]縦横のタイルの数を決める変数追加

--- TilePuzzle/Assets/Code/Scripts/Systems/TileInit.cs
+++ TilePuzzle/Assets/Code/Scripts/Systems/TileInit.cs
@@ -5,12 +5,14 @@ public class TileEvent : MonoBehaviour
     public int CorrectTileCount => _correctTileCount;
 
     [SerializeField] private int _correctTileCount;
+    [SerializeField] private int _height;
+    [SerializeField] private int _width;
 
     async void Start()
     {
     }
     
-    void InstanceTile()
+    void InstanciateTile()
     {
 
     }
