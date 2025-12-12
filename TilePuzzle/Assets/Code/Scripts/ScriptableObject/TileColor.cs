c06744892856df5bf48c32691c98855e062eedb3
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-12 18:36:46 +0900

    [add]タイルの色用のデータ

---
+++ TilePuzzle/Assets/Code/Scripts/ScriptableObject/TileColor.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileColor", menuName = "Data/TileColor")]
+public class TileColor : ScriptableObject
+{
+    public Color DefaultTileColor;
+    public Color SuccesTileColor;
+}
