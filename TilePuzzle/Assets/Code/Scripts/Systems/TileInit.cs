5e0046f388756f2438be99b83a4965b14717e709
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-12-12 20:15:40 +0900

    [update]クラスの名前変更

--- TilePuzzle/Assets/Code/Scripts/Systems/TileInit.cs
+++ TilePuzzle/Assets/Code/Scripts/Systems/TileInit.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using System.Threading.Tasks;
 
-public class TileEvent : MonoBehaviour
+public class TileInit : MonoBehaviour
 {
     public int CorrectTileCount => _correctTileCount;
 
     [SerializeField] private int _correctTileCount;
     [SerializeField] private int _height;
     [SerializeField] private int _width;
-
-    async void Start()
-    {
-    }
     
     void InstanciateTile()
     {
